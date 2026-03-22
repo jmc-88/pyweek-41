d829baffe50c315b4e0cce531af591dffe62c09d
Alexander Malmberg <alex@malmberg.org>
2026-03-22 18:01:53 +0000

    Start of rendering a heightmap.

--- main.py
+++ main.py
@@ -18,14 +18,62 @@ def main():
 
   GL.glViewport(0, 0, ScreenWidth, ScreenHeight)
   GL.glMatrixMode(GL.GL_PROJECTION)
-  GL.glFrustum(-1.6, 1.6, -1.0, 1.0, 0.1, 100.0)
+  GL.glFrustum(-0.16, 0.16, -0.1, 0.1, 0.1, 100.0)
   GL.glMatrixMode(GL.GL_MODELVIEW)
+  GL.glLoadIdentity()
+  GL.glRotate(-60, 1, 0, 0)
+  GL.glTranslate(0, 2, -1)
+
+  primitive_restart_index = 1000000000  # Arbitrary number higher than any index we plan to use.
+  GL.glPrimitiveRestartIndex(primitive_restart_index)
+  GL.glEnable(GL.GL_PRIMITIVE_RESTART)
+
+  t_w = t_h = 16
+  index_buffer = [0, t_w]
+  for x in range(t_w - 1):
+    index_buffer.append(x + 1)
+    index_buffer.append(t_w + x + 1)
+  index_buffer = numpy.array(index_buffer, dtype=numpy.int32)
+  all_parts = [index_buffer]
+  for i in range(1, t_h - 1):
+    all_parts.append(numpy.array([primitive_restart_index], dtype=numpy.int32))
+    all_parts.append(index_buffer + (i * t_w))
+  index_buffer = numpy.concat(all_parts)
+
+  index_vbo = GL.glGenBuffers(1)
+  GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, index_vbo)
+  GL.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, index_buffer, GL.GL_STATIC_DRAW)
+  GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, 0)
+
+  x, y = numpy.meshgrid(
+    numpy.linspace(-1, 1, t_w, dtype=numpy.float32),
+    numpy.linspace(-1, 1, t_h, dtype=numpy.float32))
+  z = -numpy.ones_like(x, dtype=numpy.float32)
+  positions = numpy.stack((x, y, z), -1)
+  positions_vbo = GL.glGenBuffers(1)
+  GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positions_vbo)
+  GL.glBufferData(GL.GL_ARRAY_BUFFER, positions.flatten(), GL.GL_STATIC_DRAW)
+  GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
 
   st = time.time()
   while True:
-    r = math.sin(time.time() - st) * 0.5 + 0.5
+    r = (math.sin(time.time() - st) + 1) * 0.1
     GL.glClearColor(r, 0, 0, 0)
     GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+
+    GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
+    GL.glDisable(GL.GL_CULL_FACE)
+
+    GL.glColor(1, 1, 1)
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positions_vbo)
+    GL.glEnableClientState(GL.GL_VERTEX_ARRAY)
+    GL.glVertexPointer(3, GL.GL_FLOAT, 0, None)
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+
+    GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, index_vbo)
+    GL.glDrawElements(GL.GL_TRIANGLE_STRIP, index_buffer.shape[0], GL.GL_UNSIGNED_INT, None)
+    GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, 0)
+    GL.glDisableClientState(GL.GL_VERTEX_ARRAY)
 
     pygame.display.flip()
 
