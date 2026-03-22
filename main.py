5a978273452d2458f8463712e2f0c30cc9f806d4
Alexander Malmberg <alex@malmberg.org>
2026-03-22 18:52:08 +0000

    Generate infinite terrain in chunks.

--- main.py
+++ main.py
@@ -9,6 +9,82 @@ import time
 ScreenWidth = 1440
 ScreenHeight = 810
 
+PrimitiveRestartIndex = 2000000000  # Arbitrary number higher than any vertex index we plan to use.
+
+
+TerrainResolutionX = 32
+TerrainResolutionY = 32 * 8
+TerrainWidth = 1.0  # Width of one terrain chunk in world coordinates.
+TerrainHeight = 8.0  # Height of one terrain chunk in world coordinates.
+
+class TerrainChunk:
+  def __init__(self, global_x_offset):
+    # TODO: should be possible to use a vertex shader to compute the x
+    # and y coordinates on the fly and only have to use buffers for
+    # the z values
+    x, y = numpy.meshgrid(
+      numpy.linspace(0, TerrainWidth, TerrainResolutionX, dtype=numpy.float32),
+      numpy.linspace(-TerrainHeight / 2, TerrainHeight / 2,
+                     TerrainResolutionY, dtype=numpy.float32))
+    x = x + global_x_offset  # TODO: change this so each chunk uses "local" coordinates and we line things up elsewhere so we don't get creeping accuracy issues as we move far from the origin (i.e. re-center coordinates periodically)
+
+    # TODO: generate some actually interesting terrain here, and pull in some data across chunks to make it nice and contiuous
+    z = numpy.sin(x * 8 + y * 5) * 0.05 - 0.0
+    positions = numpy.stack((x, y, z), -1)
+    positions_vbo = GL.glGenBuffers(1)
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positions_vbo)
+    GL.glBufferData(GL.GL_ARRAY_BUFFER, positions.flatten(), GL.GL_STATIC_DRAW)
+    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+    self.positions_vbo = positions_vbo
+
+  def __del__(self):
+    GL.glDeleteBuffers(1, [self.positions_vbo])
+    self.positions_vbo = -1
+
+
+class BaseTerrain:
+  def __init__(self):
+    row_buffer = [0, TerrainResolutionX]
+    for x in range(TerrainResolutionX - 1):
+      row_buffer.append(x + 1)
+      row_buffer.append(TerrainResolutionX + x + 1)
+    row_buffer = numpy.array(row_buffer, dtype=numpy.int32)
+    all_parts = [row_buffer]
+    for i in range(1, TerrainResolutionY - 1):
+      all_parts.append(numpy.array([PrimitiveRestartIndex], dtype=numpy.int32))
+      all_parts.append(row_buffer + (i * TerrainResolutionX))
+    self.index_buffer = numpy.concat(all_parts)
+
+    self.index_vbo = GL.glGenBuffers(1)
+    GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, self.index_vbo)
+    GL.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, self.index_buffer, GL.GL_STATIC_DRAW)
+    GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, 0)
+
+    self.chunks = []
+    self.next_chunk = 0
+    self.SetOffset(0)
+
+  def SetOffset(self, x):
+    while self.next_chunk < x + 4:
+      self.chunks.append(TerrainChunk(self.next_chunk))
+      self.next_chunk += 1
+    if len(self.chunks) > 6:
+      del self.chunks[:-6]
+
+  def Render(self):
+    GL.glColor(1, 1, 1)
+    GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, self.index_vbo)
+    GL.glEnableClientState(GL.GL_VERTEX_ARRAY)
+
+    for chunk in self.chunks:
+      GL.glBindBuffer(GL.GL_ARRAY_BUFFER, chunk.positions_vbo)
+      GL.glVertexPointer(3, GL.GL_FLOAT, 0, None)
+      GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+      GL.glDrawElements(GL.GL_TRIANGLE_STRIP, self.index_buffer.shape[0], GL.GL_UNSIGNED_INT, None)
+
+    GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, 0)
+    GL.glDisableClientState(GL.GL_VERTEX_ARRAY)
+
 
 def main():
   pygame.init()
@@ -19,61 +95,28 @@ def main():
   GL.glViewport(0, 0, ScreenWidth, ScreenHeight)
   GL.glMatrixMode(GL.GL_PROJECTION)
   GL.glFrustum(-0.16, 0.16, -0.1, 0.1, 0.1, 100.0)
-  GL.glMatrixMode(GL.GL_MODELVIEW)
-  GL.glLoadIdentity()
-  GL.glRotate(-60, 1, 0, 0)
-  GL.glTranslate(0, 2, -1)
 
-  primitive_restart_index = 1000000000  # Arbitrary number higher than any index we plan to use.
-  GL.glPrimitiveRestartIndex(primitive_restart_index)
+  GL.glPrimitiveRestartIndex(PrimitiveRestartIndex)
   GL.glEnable(GL.GL_PRIMITIVE_RESTART)
 
-  t_w = t_h = 32
-  index_buffer = [0, t_w]
-  for x in range(t_w - 1):
-    index_buffer.append(x + 1)
-    index_buffer.append(t_w + x + 1)
-  index_buffer = numpy.array(index_buffer, dtype=numpy.int32)
-  all_parts = [index_buffer]
-  for i in range(1, t_h - 1):
-    all_parts.append(numpy.array([primitive_restart_index], dtype=numpy.int32))
-    all_parts.append(index_buffer + (i * t_w))
-  index_buffer = numpy.concat(all_parts)
-
-  index_vbo = GL.glGenBuffers(1)
-  GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, index_vbo)
-  GL.glBufferData(GL.GL_ELEMENT_ARRAY_BUFFER, index_buffer, GL.GL_STATIC_DRAW)
-  GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, 0)
-
-  x, y = numpy.meshgrid(
-    numpy.linspace(-1, 1, t_w, dtype=numpy.float32),
-    numpy.linspace(-1, 1, t_h, dtype=numpy.float32))
-  z = numpy.sin(x * 8 + y * 5) * 0.05 - 0.0
-  positions = numpy.stack((x, y, z), -1)
-  positions_vbo = GL.glGenBuffers(1)
-  GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positions_vbo)
-  GL.glBufferData(GL.GL_ARRAY_BUFFER, positions.flatten(), GL.GL_STATIC_DRAW)
-  GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
+  base_terrain = BaseTerrain()
 
   st = time.time()
   while True:
-    r = (math.sin(time.time() - st) + 1) * 0.1
-    GL.glClearColor(r, 0, 0, 0)
+    x = (time.time() - st) * 0.5
+    base_terrain.SetOffset(x)
+    GL.glClearColor(0, 0, 0, 0)
     GL.glClear(GL.GL_COLOR_BUFFER_BIT)
+
+    GL.glMatrixMode(GL.GL_MODELVIEW)
+    GL.glLoadIdentity()
+    GL.glRotate(-60, 1, 0, 0)
+    GL.glTranslate(-x, 2, -1)
 
     GL.glPolygonMode(GL.GL_FRONT_AND_BACK, GL.GL_LINE)
     GL.glDisable(GL.GL_CULL_FACE)
 
-    GL.glColor(1, 1, 1)
-    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, positions_vbo)
-    GL.glEnableClientState(GL.GL_VERTEX_ARRAY)
-    GL.glVertexPointer(3, GL.GL_FLOAT, 0, None)
-    GL.glBindBuffer(GL.GL_ARRAY_BUFFER, 0)
-
-    GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, index_vbo)
-    GL.glDrawElements(GL.GL_TRIANGLE_STRIP, index_buffer.shape[0], GL.GL_UNSIGNED_INT, None)
-    GL.glBindBuffer(GL.GL_ELEMENT_ARRAY_BUFFER, 0)
-    GL.glDisableClientState(GL.GL_VERTEX_ARRAY)
+    base_terrain.Render()
 
     pygame.display.flip()
 
