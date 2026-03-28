c371e18fbb4b93eb5028c58d493681c2c47335fe
Gokdeniz Karadag <gokdenizk@gmail.com>
2026-03-28 10:35:45 +0000

    Move terrain into its own file

--- config.py
+++ config.py
@@ -6,3 +6,4 @@ TerrainHeight = 16.0  # Height of one terrain chunk in world coordinates.
 
 ShadowMapRes = 2048
 ShadowBias = 0.005
+PrimitiveRestartIndex = 2000000000  # Arbitrary number higher than any vertex index we plan to use.
